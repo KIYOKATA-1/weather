5a9ceb12264d22032fc15da8ecf3be761a14b39f
Pavel <112262251+KIYOKATA-1@users.noreply.github.com>
2024-01-26 03:53:12 +0600

    pavel

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ app.get("/weather", async (req, res) => {
   res.render("index", { weather, error });
 });
 
+
 const port = process.env.PORT || 3000;
 app.listen(port,'127.0.0.1', () => {
   console.log(`App is running on port ${port}`);
